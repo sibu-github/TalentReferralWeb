6fa1a43dc2b16eec9ae6413824de3875d848d935
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-15 16:08:42 +0530

    first commit

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "blucocoondigital",
+        "appName": "TalentReferralWeb",
+        "namespace": "com.blucocoondigital.TalentReferralWeb",
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web"
+    }
+}
